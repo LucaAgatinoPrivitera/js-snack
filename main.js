bdb0f6644c79fae5fab739b6e794f3358b386622
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-04-26 14:15:51 +0200

    snack 1 fatto

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const a = prompt("inserisci la prima parola");
 const b = prompt("inserisci la seconda parola");
-const word = a+b;
+let word;
+if(a.length>b.length){
+    word= a+b;
+}
+else{
+    word = b+a;
+}
 
 document.getElementById("word").innerHTML = word;
